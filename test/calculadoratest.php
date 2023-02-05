1150b12cab0278bf38a98655a68a574c2449437c
Tiinocm <tinoclase@gmail.com>
2023-02-05 18:58:10 +0100

    prueb

---
+++ test/calculadoratest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+include './src/calculadora.php';
+
+class calculadoraTest extends TestCase {
+
+    public function testSuma()
+    {
+        $objeto = new Calculadora(100, 22);
+        $this->assertEquals(122, $objeto->suma());
+    }
+
+    public function testResta()
+    {
+        $objeto = new Calculadora(100, 22);
+        $this->assertEquals(78, $objeto->resta());
+    }
+}
